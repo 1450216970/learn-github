09cc726e465f9040352bff89e15732bf91672014
Your Name <you@example.com>
2022-07-27 11:11:46 +0800

    abcg

---
+++ order-management/src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public void goToNightClass(){
+        System.out.println("我们在认真上晚自习");
+    }
+    public void BubbleLibrary(){
+        System.out.println("泡图书馆");
+    }
+    public void Class(){
+        System.out.println("在认真上课");
+    }
+    public void DoRadioExercises(){
+        System.out.println("做广播体操");
+    }
+    public void DoRadioExercises2(){
+        System.out.println("做广播体操");
+    }
+
+
+}
